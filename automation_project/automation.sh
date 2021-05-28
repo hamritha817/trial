df79d062ad78770fec25093248dc636f555a67b2
hamritha817 <hamritha817@gmail.com>
2021-05-28 09:13:51 +0000

    updated script

--- automation_project/automation.sh
+++ automation_project/automation.sh
@@ -1,7 +1,19 @@
- >> sudo yum update
- >> sudo yum install https
- >> systemctl state httpd
- >> systemctl
+ 
+#program for automation
+ sudo yum update
+ sudo yum install httpd
+ systemctl state httpd
+ systemctl
 #systemctl --type=service
 #systemctl --type=service --state=active
+ systemctl daemon -reload
+ systemctl enable httpd
+ systemctl start httpd
+ systemctl reboot
+
+ # Create tar file in /tmp/
+ /var/log/apache2/
+ ls
+ tar -cvf hamritha-httpd-log-$(date '+%d%m%Y-%H%M%S').tar access.log
+ tar -cvf hamritha-httpd-log-$(date '+%d%m%Y-%H%M%S').tar error.log
 
